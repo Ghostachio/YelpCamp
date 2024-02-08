39a5961f5f407ba50ec40b29896a3ccf2bd34627
Ghostachio <mansour.hamza2.0@gmail.com>
2024-02-08 22:34:34 +0100

    started working on athentification

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const ejsMate = require("ejs-mate");
 const AppError = require("./utils/appError");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgroundRoute = require("./routes/campgrounds");
 const reviewsRoute = require("./routes/reviews");
+const userRoute = require("./routes/users");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelpCamp");
 
@@ -22,6 +26,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
 const sessionConfig = {
   secret: "thisissupersecret",
   resave: false,
@@ -32,8 +37,17 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // setting Routes
 
 app.use((req, res, next) => {
@@ -43,6 +57,7 @@ app.use((req, res, next) => {
 });
 app.use("/campgrounds", campgroundRoute);
 app.use("/campgrounds/:id/reviews", reviewsRoute);
+app.use("/", userRoute);
 
 // setting index page
 app.get("/", async (req, res) => {
